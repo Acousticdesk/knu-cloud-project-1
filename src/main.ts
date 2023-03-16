6ace6f421ae185395bda2337e41691db2e2a6aa0
Andrii Kicha <akicha@star.global>
2023-03-16 11:37:36 -0600

    squash

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.port || 80);
+  await app.listen(process.env.PORT || 80);
 }
 bootstrap();
